bcde3c468e73b5562331a3ce4060cd81fc9b02ef
iamfaisaltyagi <iamfaisaltyagi@gmail.com>
2026-07-21 19:59:56 +0530

    restore web routes

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,8 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\WorkoutController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/dashboard', DashboardController::class)
@@ -32,35 +21,73 @@ Route::get('/dashboard', DashboardController::class)
     ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('tasks', TaskController::class)->except(['create', 'show', 'edit']);
-    Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
-    Route::post('/tasks/{task}/duplicate', [TaskController::class, 'duplicate'])->name('tasks.duplicate');
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
 
-    Route::resource('diet-items', DietItemController::class)->except(['create', 'show', 'edit']);
-    Route::patch('/diet-items/{diet_item}/toggle', [DietItemController::class, 'toggle'])->name('diet-items.toggle');
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
 
-    Route::resource('workouts', WorkoutController::class)->except(['create', 'show', 'edit']);
-    Route::patch('/workouts/{workout}/toggle', [WorkoutController::class, 'toggle'])->name('workouts.toggle');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
 
-    Route::resource('habits', HabitController::class)->except(['create', 'show', 'edit']);
-    Route::patch('/habits/{habit}/toggle-completion', [HabitController::class, 'toggleCompletion'])->name('habits.toggle-completion');
 
-    Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar.index');
+    Route::resource('tasks', TaskController::class)
+        ->except(['create', 'show', 'edit']);
+
+    Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggle'])
+        ->name('tasks.toggle');
+
+    Route::post('/tasks/{task}/duplicate', [TaskController::class, 'duplicate'])
+        ->name('tasks.duplicate');
+
+
+    Route::resource('diet-items', DietItemController::class)
+        ->except(['create', 'show', 'edit']);
+
+    Route::patch('/diet-items/{diet_item}/toggle', [DietItemController::class, 'toggle'])
+        ->name('diet-items.toggle');
+
+
+    Route::resource('workouts', WorkoutController::class)
+        ->except(['create', 'show', 'edit']);
+
+    Route::patch('/workouts/{workout}/toggle', [WorkoutController::class, 'toggle'])
+        ->name('workouts.toggle');
+
+
+    Route::resource('habits', HabitController::class)
+        ->except(['create', 'show', 'edit']);
+
+    Route::patch('/habits/{habit}/toggle-completion', [HabitController::class, 'toggleCompletion'])
+        ->name('habits.toggle-completion');
+
+
+    Route::get('/calendar', [CalendarController::class, 'index'])
+        ->name('calendar.index');
 });
+
 
 Route::middleware(['auth', 'verified', 'admin'])
     ->prefix('admin')
     ->name('admin.')
     ->group(function () {
-        Route::get('/dashboard', AdminDashboardController::class)->name('dashboard');
-        Route::get('/users', [AdminUserController::class, 'index'])->name('users.index');
-        Route::patch('/users/{user}', [AdminUserController::class, 'update'])->name('users.update');
-        Route::get('/settings', [AdminSettingController::class, 'index'])->name('settings.index');
-        Route::patch('/settings', [AdminSettingController::class, 'update'])->name('settings.update');
-});
+
+        Route::get('/dashboard', AdminDashboardController::class)
+            ->name('dashboard');
+
+        Route::get('/users', [AdminUserController::class, 'index'])
+            ->name('users.index');
+
+        Route::patch('/users/{user}', [AdminUserController::class, 'update'])
+            ->name('users.update');
+
+        Route::get('/settings', [AdminSettingController::class, 'index'])
+            ->name('settings.index');
+
+        Route::patch('/settings', [AdminSettingController::class, 'update'])
+            ->name('settings.update');
+    });
+
 
 require __DIR__.'/auth.php';
